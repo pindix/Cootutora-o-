242d02df127707034c14e3e8784eec80c41337a0
pindix <pindixy@gmail.com>
2025-10-22 21:45:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function solicitar(){
 
     if (pag > ""){
         valor.innerHTML = `É necessário pagar 50% antes de começarmos o trabalho.  ${Math.round(((pag*200)*0.35)*0.5)} KZ se quiser continuar com as ${pag} páginas que digitou.`;
-    } else {valor.innerHTML = "Énecessário pagar 50% antes de começarmos o trabalho."}
+    } else {valor.innerHTML = "É necessário pagar 50% antes de começarmos o trabalho."}
 }
 
 function chamar(){
